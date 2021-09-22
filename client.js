b251211b4ad05e9c50c3c7a816fa7c3b67d7828c
Jordan Cronier <jcronier777@gmail.com>
2021-09-22 19:30:28 +0000

    Added command line inputs

--- client.js
+++ client.js
@@ -3,8 +3,8 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: 50541,
+    host: "135.23.223.133",
+    port: 50542,
   });
 
   // interpret incoming data as text
@@ -13,11 +13,18 @@ const connect = function () {
   conn.on('data', (data) => {
     console.log('Message from server: ', data);
   });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: JWC");
+  });
+
+  conn.on("end", () => {
+    console.log("Disconnected from server.");
+    process.exit();
+  })
   
   return conn;
 };
-
-console.log("Connecting ...");
-connect();
 
 module.exports = { connect };
--- play.js
+++ play.js
@@ -1,3 +1,21 @@
 const { connect } = require("./client");
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", key => handleUserInput(key));
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    console.log("Thanks for playing");
+    process.exit();
+  }
+};
+
+setupInput();
 connect();
