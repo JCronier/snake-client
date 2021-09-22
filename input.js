9fd32959290cffc8b7650e3d659afd52f6ccae8e
Jordan Cronier <jcronier777@gmail.com>
2021-09-22 19:48:25 +0000

    Added ability to move ingame

--- input.js
+++ input.js
@@ -1,7 +1,11 @@
 // input.js
 
+const { stdin } = require("process");
+
+let connection;
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -14,6 +18,14 @@ const handleUserInput = function(key) {
   if (key === "\u0003") {
     console.log("Thanks for playing");
     process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
   }
 };
 
